2f1eac7ba0e4c8fcd8b65075466f03fc138994f2
Emily Langworth <e24murph@gmail.com>
2022-12-20 21:37:58 -0800

    created centiment db and added user, category, post, topic models

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS centiment_db;
+
+CREATE DATABASE centiment_db;
+
+USE centiment_db;
---
+++ models/Topic.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('..config/connection');
+
+class Topic extends Model {}
+
+Topic.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id',
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: now(),
+        },
+        total_shares: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 100000,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'topic',
+    }
+);
+
+module.exports = Topic;
