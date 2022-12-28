598eab75689c8ef426fed91cb9bd9ea6b6c6524c
MAYC 6976 <isayah@fungeapp.com>
2022-12-27 19:47:04 -0800

    Updated the create topic page to look nicer. Need more backend functionality to continue.

--- controllers/api/topic.js
+++ controllers/api/topic.js
@@ -1,23 +1,24 @@
 const { Router } = require('express');
 const auth = require('../../middleware/auth');
-const { Topic } = require("./../../models");
+const { Topic } = require('./../../models');
 const topicRouter = new Router();
 
 topicRouter.post('/', auth, async (req, res) => {
-  const { name, description, price } = req.body
-  try {
-  const newTopic = await Topic.create({
-    name,
-    description,
-    price,
-    user_id: req.user.id
-    });
-    res.status(200).json({
-      id: newTopic.id,
-    });
+    const { topic_name, description, price } = req.body;
+    console.log(topic_name, description, price);
+    try {
+        const newTopic = await Topic.create({
+            topic_name,
+            description,
+            price,
+            user_id: req.user.id,
+        });
+        res.status(200).json({
+            id: newTopic.id,
+        });
     } catch (err) {
-    res.status(400).json(err);
-  }
+        res.status(400).json(err);
+    }
 });
 
-module.exports = topicRouter
+module.exports = topicRouter;
