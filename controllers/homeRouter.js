040e0953136b269f6efa67a9a5d005d88d49822c
Frank Nguyen <frank@ultra.me>
2022-12-29 11:22:01 -0800

    added more topic routes

--- controllers/homeRouter.js
+++ controllers/homeRouter.js
@@ -15,6 +15,18 @@ homeRouter.get("/login", async (req, res) => {
   res.render("login");
 });
 
+// router to display profile page
+homeRouter.get("/user", auth, async (req, res) => {
+
+  const user = req.user.get({ plain: true });
+  const Buffer = require('buffer').Buffer;
+
+  const imageData = Buffer.from(user.avatar).toString('base64');
+  const imageURI = `data:image/jpeg;base64,${imageData}`;
+  res.render('profile', { user, imageURI });
+});
+
+// Topic routes
 homeRouter.get("/topic", auth, async (req, res) => {
   const plainUser = req.user.get({ plain: true });
 
@@ -31,15 +43,49 @@ homeRouter.get("/topic", auth, async (req, res) => {
     topics: plainTopics,
     });
 });
-// router to display profile page
-homeRouter.get("/user", auth, async (req, res) => {
 
-  const user = req.user.get({ plain: true });
-  const Buffer = require('buffer').Buffer;
+// Get single topic
+homeRouter.get("/topic/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  const topic = await Topic.findByPk(id);
 
-  const imageData = Buffer.from(user.avatar).toString('base64');
-  const imageURI = `data:image/jpeg;base64,${imageData}`;
-  res.render('profile', { user, imageURI });
-});
+  if (!topic) {
+    res.status(404).end("No such topic");
+    return;
+  }
+
+  const topicSimple = topic.get({ simple: true });
+
+  res.render('single-topic', {
+    topic: topicSimple,
+  });
+})
+// Edit single topic
+homeRouter.get("/topic/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const topic = await Topic.findByPk(id);
+  if (!topic) {
+    res.status(404).end("No such topic");
+    return;
+  }
+  const topicSimple = topic.get({ simple: true });
+  res.render('edit-topic', {
+    topic: topicSimple,
+  });
+})
+
+// Post routes
+homeRouter.get("topic/:id/post/new", async (req, res) => {
+  const { id } = req.params;
+  const topic = await Topic.findByPk(id);
+  if (!topic) {
+    res.status(404).end("No such topic");
+    return;
+  }
+  const topicSimple = topic.get({ simple: true });
+  res.render('new-post', {
+    topic: topicSimple,
+  });
+})
 
 module.exports = homeRouter;
