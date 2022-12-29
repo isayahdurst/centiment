5a11233cab6ab88ff220447c50e74ad36657990e
Emily Langworth <e24murph@gmail.com>
2022-12-28 19:26:30 -0800

    added ank and bid models

---
+++ models/Ask.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Ask extends Model {}
+
+Ask.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        shares: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        topic_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'topic',
+                key: 'id',
+            }
+        },
+        ask_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        expiration_date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW, //will need to be validated to make sure this is 7 days in the future
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'ask',
+    },
+);
+
+module.exports = Ask;
